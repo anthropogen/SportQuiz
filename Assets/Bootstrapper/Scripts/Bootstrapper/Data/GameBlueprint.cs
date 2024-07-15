d81859591f77b8e483efaede055fa215e5adf458
Aleksandr Komarov <anthropogen96@gmail.com>
2024-07-15 20:45:03 +0300

    parse json to blueprint

--- Assets/Bootstrapper/Scripts/Bootstrapper/Data/GameBlueprint.cs
+++ Assets/Bootstrapper/Scripts/Bootstrapper/Data/GameBlueprint.cs
@@ -1,9 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Bootstrapper.Data
 {
   [CreateAssetMenu(fileName = "GameBlueprint", menuName = "New GameBlueprint")]
   public class GameBlueprint : ScriptableObject
   {
+    public Root root;
+  }
+
+  [System.Serializable]
+  public class Category
+  {
+    public string name;
+    public string rules;
+    public List<Level> levels;
+  }
+
+  [System.Serializable]
+  public class Level
+  {
+    public string question;
+    public List<string> options;
+    public string answer;
+  }
+
+  [System.Serializable]
+  public class Root
+  {
+    public List<Category> categories;
   }
 }
---
+++ Assets/Editor/GameBlueprintEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization.Json;
+using Bootstrapper.Data;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameBlueprint))]
+public class GameBlueprintEditor : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    base.OnInspectorGUI();
+    if (GUILayout.Button("Parse"))
+    {
+      var json = Resources.Load<TextAsset>("qz");
+      var root = JsonUtility.FromJson<Root>(json.text);
+      var bl = target as GameBlueprint;
+      bl.root = root;
+      EditorUtility.SetDirty(target);
+    }
+  }
+}
