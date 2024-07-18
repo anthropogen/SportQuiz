0e8c1a5aa7f87696770643a013e7f14afb697bd8
Komarov Alexander <anthropogen96@gmail.com>
2024-07-18 17:21:34 +0300

    result screen

--- Assets/App/Scripts/Screens/ResultScreen.cs
+++ Assets/App/Scripts/Screens/ResultScreen.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Bootstrapper.Data;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 using Screen = Bootstrapper.UI.Screen;
 
@@ -6,7 +8,19 @@ public class ResultScreen : Screen
 {
   [SerializeField] private Button menuBtn;
   [SerializeField] private Button playBtn;
+  [SerializeField] private Slider slider;
+  [SerializeField] private TMP_Text resultTxt;
+  [SerializeField] private Image icon;
+  [SerializeField] private TMP_Text title;
   [SerializeField] private ResultSystem resultSystem;
+
+  public void Init(Category category, int current, int max)
+  {
+    slider.value = current / (float)max;
+    resultTxt.text = $"Right {current} out of {max}";
+    icon.sprite = category.icon;
+    title.text = $"Rules \nof the \n{category.name}";
+  }
 
   protected override void Subscribe()
   {
--- Assets/App/Scripts/Systems/ResultSystem.cs
+++ Assets/App/Scripts/Systems/ResultSystem.cs
@@ -1,6 +1,12 @@
 ﻿using Bootstrapper.StateMachine;
+using UnityEngine;
 
 public class ResultSystem : GameSystem
 {
-  
+  [SerializeField] private ResultScreen resultScreen;
+
+  internal override void EnterState()
+  {
+    resultScreen.Init(gameData.Category, gameData.RightAnswers, Helper.GetQuestionsNumFor(gameData.Difficult));
+  }
 }
